9e6009a27be23d329c006554089b6a6fa9dd70fc
Lyubomira Lyubomirova <llyubomirova@soft2run.com>
2024-10-01 14:01:12 +0300

    changes in worklist

--- apps/worklist/src/app/app.routes.ts
+++ apps/worklist/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+
+export const appRoutes: Route[] = [
+  {
+    path: 'ui-components',
+    loadComponent: () =>
+      import('@angular-monorepo/ui-components').then((m) => m.UiComponentsComponent),
+  }
+];
--- libs/json-forms-renderers/src/lib/json-forms-renderers/json-forms-renderers.component.ts
+++ libs/json-forms-renderers/src/lib/json-forms-renderers/json-forms-renderers.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import {UiComponentsComponent} from '@angular-monorepo/ui-components'
+ 
 @Component({
   selector: 'lib-json-forms-renderers',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, UiComponentsComponent],
   templateUrl: './json-forms-renderers.component.html',
   styleUrl: './json-forms-renderers.component.css',
 })
